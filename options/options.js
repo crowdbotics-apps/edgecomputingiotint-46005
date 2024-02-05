bb310cff97b2a98b252c07ecd94d1275167427c2
Crowdbotics <team@crowdbotics.com>
2024-02-05 22:04:20 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "edgecomputingiotint-46005",
+  url: "https://edgecomputingiotint-46005.botics.co",
+  api: "https://edgecomputingiotint-46005.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
